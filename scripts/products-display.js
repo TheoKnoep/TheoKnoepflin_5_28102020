501a0e63843e68212bc73cd574cbe7ed9363857c
Théo Knoepflin <theo.knoepflin@gmail.com>
2020-11-02 16:49:26 +0100

    Page de la liste des produts chargée à partir du serveur

---
+++ scripts/products-display.js
@@ -0,0 +1,20 @@
+let productsList = document.getElementById("products-list"); 
+
+let url = "http://localhost:3000/api/teddies"; 
+
+fetch(url)
+	.then(response => response.json())
+    		.then(response => {
+			for (let i in response) {
+				let newCard = document.createElement("a"); 
+				newCard.setAttribute("href", "produit.html?id=" + response[i]._id); 
+				newCard.innerHTML = `<article class="product-card">
+										<h2 class="product-card__name heading">${response[i].name}</h2>
+										<div class="product-card__image">
+											<img src="${response[i].imageUrl}" />
+										</div>
+									</article>`
+				productsList.appendChild(newCard); 
+			}
+		})
+ 	.catch(error => alert("Erreur : " + error));
