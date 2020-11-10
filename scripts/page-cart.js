12bf68fe6de575cfa99d5c21228227bc53e7e21a
Théo Knoepflin <theo.knoepflin@gmail.com>
2020-11-10 16:07:39 +0100

    Version intermédiaire POST fetch panier

--- scripts/page-cart.js
+++ scripts/page-cart.js
@@ -69,6 +69,8 @@ confirmButton.addEventListener('click', function(e) { //ou bien événement 'sub
 		e.preventDefault(); 
 	}
 
+	e.preventDefault(); //désactivation du submit pour tester le fonctionnement
+
 	//on génère l'objet contact à partir des données du formulaire : 
 	let contact = {
 		firstName: formContent[0].value,
@@ -77,31 +79,31 @@ confirmButton.addEventListener('click', function(e) { //ou bien événement 'sub
 		city: formContent[3].value,
 		email: formContent[4].value
 	}; 
+	console.log(JSON.stringify(contact)); 
 
 	//on récupère les ID des produits dans un array de strings : 
 	let products = []; 
 	for (let i in myCart.content) {
 		products.push(myCart.content[i]._id); 
 	}
-	console.log(products); 
-
-	//générer un numéro de commande : 
-	const d = new Date; 
-	const order_id = `${d.getFullYear()}${d.getMonth() + 1}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`; 
 
 	//on gère la requête d'envoi d'order_id, contact, prodcuts : 
 	// post body data 
 	const order = {
 		contact: contact, 
 		products: products, 
-		order_id: order_id
 	};
+
 	//request options
 	const options = {
 		method: 'POST',
-		body: JSON.stringify(order)
+		body: order 
 	}
 	fetch("http://localhost:3000/api/teddies/order", options)
-		.then(res => res.json())
-			.then(res => console.log(res));
+		.then(response => response.json())
+			.then(response => console.log(response))
+		.catch(error => console.log(error)); 
+
+	//on réinitialise le contenu du panier : 
+	// myCart.toEmptyCart(); 
 }); 
--- scripts/page-confirmation.js
+++ scripts/page-confirmation.js
@@ -1,26 +1,15 @@
 let myCart = new Cart(); 
-/*
-// post body data 
-const order = {
-	contact: contactInfo, 
-	products: myCart.content, 
-	orderID: orderID
-};
 
-// request options
-const options = {
-	method: 'POST',
-	body: JSON.stringify(order)
-}
-// on envoie la requête POST : 
-fetch("http://localhost:3000/api/teddies/order", options)
-	.then(res => res.json())
-	.then(res => console.log(res));
-*/
+
+fetch("http://localhost:3000/api/teddies/order")
+	.then(response => response.json())
+    		.then(response => {
+			    console.log(response); 
+		})
+ 	.catch(error => console.log(error));
+
 
 //on génère la réponse HTML : 
 //const orderFeedback = document.getElementById("order-feedback");
-//orderFeedback.textContent = `Votre commande n° ${orderID} est validée ! Nous allons la traiter au plus vite`; 
+//orderFeedback.textContent = `Votre commande n° ${response.order_id} est validée ! Nous allons la traiter au plus vite`; 
 
-//on réinitialise le contenu du panier : 
-// myCart.toEmptyCart(); 
